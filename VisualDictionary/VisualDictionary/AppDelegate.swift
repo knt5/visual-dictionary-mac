39cd3b9fda1ee464a9506dd59d6cd8f049b4c35b
knt5 <knt5@users.noreply.github.com>
2016-08-19 21:15:38 +0900

    Create xcode project

---
+++ VisualDictionary/VisualDictionary/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VisualDictionary
+//
+//  Created by apple on 2016/08/19.
+//  Copyright © 2016年 Kenta Motomura. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ VisualDictionary/VisualDictionary/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VisualDictionary
+//
+//  Created by apple on 2016/08/19.
+//  Copyright © 2016年 Kenta Motomura. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

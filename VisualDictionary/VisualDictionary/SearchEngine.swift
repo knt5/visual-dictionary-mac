81491d4174f8e910ee006c99d47e231651b7576c
knt5 <knt5@users.noreply.github.com>
2016-08-23 22:59:38 +0900

    Add SearchEngine class

---
+++ VisualDictionary/VisualDictionary/SearchEngine.swift
@@ -0,0 +1,21 @@
+//
+//  SearchEngine.swift
+//  VisualDictionary
+//
+//  Created by apple on 2016/08/23.
+//  Copyright © 2016年 Kenta Motomura. All rights reserved.
+//
+
+import Foundation
+
+class SearchEngine {
+	let name: String
+	let url: String
+	let id: String
+	
+	init(name: String, url: String, id: String = "") {
+		self.name = name
+		self.url = url
+		self.id = id
+	}
+}

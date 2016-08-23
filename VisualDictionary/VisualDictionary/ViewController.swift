d3dc910dbbc40aef5227331efc751d8cc01868fb
knt5 <knt5@users.noreply.github.com>
2016-08-23 23:01:35 +0900

    Update UI and add search engine PopUpButton menu generation logic

--- VisualDictionary/VisualDictionary/ViewController.swift
+++ VisualDictionary/VisualDictionary/ViewController.swift
@@ -9,16 +9,40 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+	@IBOutlet var mainView: NSView!
+	@IBOutlet weak var searchField: NSSearchField!
+	@IBOutlet weak var searchEnginePopUpButton: NSPopUpButton!
+	@IBOutlet weak var imageSearchEnginePopUpButton: NSPopUpButton!
+	
+	private let config: Config = Config()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
-	}
-
-	override var representedObject: AnyObject? {
-		didSet {
-		// Update the view, if already loaded.
+		
+		config.getSearchEngines().forEach { (engine : SearchEngine) -> () in
+			searchEnginePopUpButton.addItemWithTitle(engine.name)
+		}
+		
+		config.getImageSearchEngines().forEach { (engine : SearchEngine) -> () in
+			imageSearchEnginePopUpButton.addItemWithTitle(engine.name)
 		}
 	}
+	
+	override var representedObject: AnyObject? {
+		didSet {
+			// Update the view, if already loaded.
+		}
+	}
+	
+	private func getSearchEngine() -> SearchEngine {
+		return config.getSearchEngines()[searchEnginePopUpButton.indexOfSelectedItem]
+	}
+	
+	private func getImageSearchEngine() -> SearchEngine {
+		return config.getImageSearchEngines()[imageSearchEnginePopUpButton.indexOfSelectedItem]
+	}
+	
+	@IBAction func onSearchAction(sender: AnyObject) {
+	}
+	
 }

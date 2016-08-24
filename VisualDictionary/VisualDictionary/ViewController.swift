8e4b3e288d39d990243892c41cfece14e7374cd9
knt5 <knt5@users.noreply.github.com>
2016-08-24 22:18:55 +0900

    Add input keyword speaking

--- VisualDictionary/VisualDictionary/ViewController.swift
+++ VisualDictionary/VisualDictionary/ViewController.swift
@@ -17,7 +17,11 @@ class ViewController: NSViewController {
 	@IBOutlet weak var dictionaryWebView: WebView!
 	@IBOutlet weak var imageWebView: WebView!
 	
+	override var representedObject: AnyObject? { didSet {} }
+	//override var acceptsFirstResponder: Bool { return true }
+	
 	private let config: Config = Config()
+	private let textView: NSTextView = NSTextView(frame: NSMakeRect(-1, -1, 0, 0))
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -31,22 +35,24 @@ class ViewController: NSViewController {
 		}
 	}
 	
-	override var representedObject: AnyObject? {
-		didSet {
-			// Update the view, if already loaded.
-		}
-	}
-	
+	// Get a selected search engine for dictionary
 	private func getSearchEngine() -> SearchEngine {
 		return config.getSearchEngines()[searchEnginePopUpButton.indexOfSelectedItem]
 	}
 	
+	// Get a selected image search engine
 	private func getImageSearchEngine() -> SearchEngine {
 		return config.getImageSearchEngines()[imageSearchEnginePopUpButton.indexOfSelectedItem]
 	}
 	
 	@IBAction func onSearchAction(sender: AnyObject) {
 		if (searchField.stringValue.characters.count > 0) {
+			// Start speaking keyword
+			if (config.speaking) {
+				textView.string = searchField.stringValue
+				textView.startSpeaking(nil)
+			}
+			
 			// Get keyword
 			let key: String = searchField.stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
 			

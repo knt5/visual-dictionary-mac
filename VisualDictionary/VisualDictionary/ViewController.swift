f1a3f1bebeaae3e6f5405fbbbdd5172729e3d986
knt5 <knt5@users.noreply.github.com>
2016-08-26 00:09:46 +0900

    Organize comments

--- VisualDictionary/VisualDictionary/ViewController.swift
+++ VisualDictionary/VisualDictionary/ViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 import WebKit
 
 class ViewController: NSViewController {
+	// UI
 	@IBOutlet var mainView: NSView!
 	@IBOutlet weak var searchField: NSSearchField!
 	@IBOutlet weak var searchEnginePopUpButton: NSPopUpButton!
@@ -17,40 +18,60 @@ class ViewController: NSViewController {
 	@IBOutlet weak var dictionaryWebView: WebView!
 	@IBOutlet weak var imageWebView: WebView!
 	
-	override var representedObject: AnyObject? { didSet {} }
-	//override var acceptsFirstResponder: Bool { return true }
-	
+	// App config
 	private let config: Config = Config()
+	
+	// TextView for speech
 	private let textView: NSTextView = NSTextView(frame: NSMakeRect(-1, -1, 0, 0))
 	
+	//============================================================
+	override var representedObject: AnyObject? {
+		didSet {
+		}
+	}
+	
+	//============================================================
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		//============================================================
+		// Setup UI
+		
+		// Add search engines to PopUpButton
 		config.getSearchEngines().forEach { (engine : SearchEngine) -> () in
 			searchEnginePopUpButton.addItemWithTitle(engine.name)
 		}
 		
+		// Add image search engines to PopUpButton
 		config.getImageSearchEngines().forEach { (engine : SearchEngine) -> () in
 			imageSearchEnginePopUpButton.addItemWithTitle(engine.name)
 		}
 		
-		// Set preferences to WebView
+		//============================================================
+		// Set preferences of WebView
+		
+		// Set preferences ID to separate preferences of WebViews
 		dictionaryWebView.preferencesIdentifier = "dictionary"
 		imageWebView.preferencesIdentifier = "image"
+		
+		// Set preferences to block Ads
 		dictionaryWebView.preferences.javaScriptEnabled = false
 		dictionaryWebView.preferences.loadsImagesAutomatically = false
 	}
 	
-	// Get a selected search engine for dictionary
+	//============================================================
+	// Get selected search engine for dictionary
 	private func getSearchEngine() -> SearchEngine {
 		return config.getSearchEngines()[searchEnginePopUpButton.indexOfSelectedItem]
 	}
 	
-	// Get a selected image search engine
+	//============================================================
+	// Get selected image search engine
 	private func getImageSearchEngine() -> SearchEngine {
 		return config.getImageSearchEngines()[imageSearchEnginePopUpButton.indexOfSelectedItem]
 	}
 	
+	//============================================================
 	@IBAction func onSearchAction(sender: AnyObject) {
 		if (searchField.stringValue.characters.count > 0) {
 			// Start speaking keyword (Recommended speech voice is "Samantha")
